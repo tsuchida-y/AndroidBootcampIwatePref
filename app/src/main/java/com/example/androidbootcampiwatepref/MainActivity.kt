8dfc518b4886e61d0e349fc13a9c39323ef538ec
tsuchida <yu41926t.tutida@icloud.com>
2024-09-27 01:40:21 +0900

    secondcommit

--- app/src/main/java/com/example/androidbootcampiwatepref/MainActivity.kt
+++ app/src/main/java/com/example/androidbootcampiwatepref/MainActivity.kt
@@ -73,69 +73,14 @@ data class Greeting(
 )
 
 
-class GreetingViewModel : ViewModel() { //ViewModelクラスを継承している
-    //配列を作成し、mutableStateListOfで監視している
-    val greetings = mutableStateListOf<Greeting>()
 
-    // 例: 初期値を設定
-    init {
-        greetings.addAll(listOf(
-            Greeting("名前"),
-            Greeting("出身地"),
-            Greeting("職業/学業"),
-            Greeting("趣味"),
-            Greeting("好きな食べ物"),
-            Greeting("好きなスポーツ"),
-            Greeting("好きな国"),
-            Greeting("将来の目標"),
-            Greeting("興味のある分野"),
-            Greeting("今取り組んでいること"),
-            Greeting("好きな映画"),
-            Greeting("好きな音楽"),
-            Greeting("好きな場所"),
-            Greeting("得意なこと"),
-        ))
-    }
-
-    //追加
-    fun serializeGreetings(): String {
-        return Json.encodeToString(greetings.map { it.copy() }) // copyメソッドを使って状態を保持
-    }
-
-
-// デシリアライズ
-fun deserializeGreetings(jsonString: String) {
-    try {
-        val deserializedList = Json.decodeFromString<List<Greeting>>(jsonString)//JSON文字列をGreetingのリストに変換
-        greetings.addAll(deserializedList)//greetingsリストに追加
-    } catch (e: Exception) {//tryでエラーが発生したらここに来る
-    }
-}
-
-    //新しい自己紹介項目をリストに追加するメソッド
-    fun addGreeting(newText: String,mainViewModel: MainViewModel) {
-        val newGreeting = Greeting(newText)
-        greetings.add(newGreeting) // リストをクリアせずに新しい項目を追加
-
-        val jsonData = serializeGreetings()
-        Log.d("GreetingViewModel", "Serialized greetings: $jsonData")
-        mainViewModel.setGreetingData(jsonData) // データストアへ同期
-    }
-
-    fun initializeCounts(counts: List<Int>) {
-        for (i in greetings.indices) {
-            greetings[i].count = counts[i]
-        }
-    }
-
-    }
 
 class AppDataStore(//DataStoreの実装
     private val context: Context
 ) {
     companion object {
         private const val USER_PREFERENCES_NAME = "user_preferences"
-        private const val MAX_ITEMS = 14 // 項目の数
+        private const val MAX_ITEMS = 20 // 項目の数
         val COUNT_KEYS = List(MAX_ITEMS) { index -> intPreferencesKey("count_$index") }
     }
 
@@ -217,15 +162,84 @@ fun MyApp(navController: NavHostController = rememberNavController(),
           viewModel: GreetingViewModel = viewModel(),
           mainViewModel: MainViewModel) {
     // GreetingViewModel の状態を remember で保持
-    val greetingViewModel: GreetingViewModel = viewModel() // Composable 関数の外で保持
+    //val greetingViewModel: GreetingViewModel = viewModel() // Composable 関数の外で保持
 
     NavHost(//ここを使ってナビゲーションを管理する(home画面とdetail画面)
         navController = navController,
         startDestination = "home"
     ) {
         composable("home") { HomeScreen(navController, viewModel, mainViewModel) }
-        composable("detail") { DetailScreen(navController, viewModel, mainViewModel) }
+        composable("detail") { DetailScreen(navController, viewModel, mainViewModel) }// Ensure DetailScreen is defined above
     }
+}
+
+class GreetingViewModel : ViewModel() { //ViewModelクラスを継承している
+    //配列を作成し、mutableStateListOfで監視している
+    val greetings = mutableStateListOf<Greeting>()
+
+    // 例: 初期値を設定
+    init {
+        greetings.addAll(listOf(
+            Greeting("名前"),
+            Greeting("出身地"),
+            Greeting("職業/学業"),
+            Greeting("趣味"),
+            Greeting("好きな食べ物"),
+            Greeting("好きなスポーツ"),
+            Greeting("好きな国"),
+            Greeting("将来の目標"),
+            Greeting("興味のある分野"),
+            Greeting("今取り組んでいること"),
+            Greeting("好きな映画"),
+            Greeting("好きな音楽"),
+            Greeting("好きな場所"),
+            Greeting("得意なこと"),
+        ))
+    }
+
+    //追加
+    fun serializeGreetings(): String {
+        return Json.encodeToString(greetings.map { it.copy() }) // copyメソッドを使って状態を保持
+    }
+
+
+    // デシリアライズ
+    fun deserializeGreetings(jsonString: String) {
+        try {
+            val deserializedList = Json.decodeFromString<List<Greeting>>(jsonString)//JSON文字列をGreetingのリストに変換
+            deserializedList.forEach { greeting ->
+                if (!greetings.any { it.text == greeting.text }) {
+                    greetings.add(greeting)
+                }
+            }
+
+        } catch (e: Exception) {//tryでエラーが発生したらここに来る
+            Log.e("GreetingViewModel", "Error deserializing JSON", e)
+        }
+    }
+
+    //新しい自己紹介項目をリストに追加するメソッド
+    fun addGreeting(newText: String,mainViewModel: MainViewModel) {//自己紹介項目のテキスト,MainViewModelのインスタンス
+        val newGreeting = Greeting(newText)//Greetingオブジェクトを生成
+        greetings.add(newGreeting) //新しい項目を追加
+        saveData(mainViewModel)
+        val jsonData = serializeGreetings()
+        mainViewModel.setGreetingData(jsonData) // データストアへ同期
+    }
+
+    fun saveData(mainViewModel: MainViewModel) {
+        val jsonData = serializeGreetings()
+        mainViewModel.setGreetingData(jsonData)
+    }
+
+    fun initializeCounts(counts: List<Int>) {
+        for (i in greetings.indices) {
+            if (i < counts.size) {
+                greetings[i].count = counts[i]
+            }
+        }
+    }
+
 }
 
 @Composable
@@ -238,195 +252,199 @@ fun HomeScreen(navController: NavHostController, viewModel: GreetingViewModel,ma
         mainViewModel.greetingData.collect { jsonData: String? ->
             // 取得したデータがnullまたは空でない場合
             if (!jsonData.isNullOrBlank()) {
-                try {
-                    // JSONデータをデシリアライズして、greetingsに設定
-                    viewModel.deserializeGreetings(jsonData)
-                    Log.d("HomeScreen", "Deserialized successfully")
-                } catch (e: Exception) {
-                    // デシリアライズエラーをログに出力
-                    Log.e("HomeScreen", "Deserialization error: ${e.message}")
-                }
+                // JSONデータをデシリアライズして、greetingsに設定
+                viewModel.deserializeGreetings(jsonData)
             } else if (viewModel.greetings.isEmpty()) { // 初期データがない場合のみ追加
-                Log.d("HomeScreen", "Adding initial greetingsあ")
                 // 初期データをgreetingsに追加
-                viewModel.greetings.addAll(listOf(
-                    Greeting("名前"),
-                    Greeting("出身地"),
-                    Greeting("職業/学業"),
-                    Greeting("趣味"),
-                    Greeting("好きな食べ物"),
-                    Greeting("好きなスポーツ"),
-                    Greeting("好きな国"),
-                    Greeting("将来の目標"),
-                    Greeting("興味のある分野"),
-                    Greeting("今取り組んでいること"),
-                    Greeting("好きな映画"),
-                    Greeting("好きな音楽"),
-                    Greeting("好きな場所"),
-                    Greeting("得意なこと"),
-                ))
+                viewModel.greetings.addAll(
+                    listOf(
+                        Greeting("名前"),
+                        Greeting("出身地"),
+                        Greeting("職業/学業"),
+                        Greeting("趣味"),
+                        Greeting("好きな食べ物"),
+                        Greeting("好きなスポーツ"),
+                        Greeting("好きな国"),
+                        Greeting("将来の目標"),
+                        Greeting("興味のある分野"),
+                        Greeting("今取り組んでいること"),
+                        Greeting("好きな映画"),
+                        Greeting("好きな音楽"),
+                        Greeting("好きな場所"),
+                        Greeting("得意なこと"),
+                    )
+                )
+            }
+            // カウントを初期化
+            mainViewModel.counts.collect { counts ->
+                viewModel.initializeCounts(counts) // countsをgreetingsに適用
             }
         }
     }
 
 
-
-
-    // 終了時にデータを保存
-    DisposableEffect(Unit) {
-        onDispose {
-            val jsonData = viewModel.serializeGreetings()
-            Log.d("HomeScreen", "Saving greetings data: $jsonData")
-            mainViewModel.setGreetingData(jsonData)
-        }
-    }
-
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
-                    navController.navigate("detail")
-                    Log.d("HomeScreen", "FloatingActionButton clicked")
-                          },
-                containerColor = MaterialTheme.colorScheme.primary
-            ) {
-                Icon(Icons.Filled.Add, contentDescription = "Add")
+        // 終了時にデータを保存
+        DisposableEffect(Unit) {
+            onDispose {
+                val jsonData = viewModel.serializeGreetings()
+                Log.d("HomeScreen", "Saving greetings data: $jsonData")
+                mainViewModel.setGreetingData(jsonData)
             }
-        },
-        content = { padding ->
-            Log.d("HomeScreen", "Scaffold content rendering")
-            Column(
-                modifier = Modifier
-                    .padding(padding)
-                    .padding(bottom = 64.dp) // ボタンとの重なりを避けるために下に余白を追加
-            ) {
-                Text(
-                    text = "自己紹介の項目",
-                    style = MaterialTheme.typography.headlineMedium,
+        }
+
+        Scaffold(
+            floatingActionButton = {
+                FloatingActionButton(
+                    onClick = {
+                        navController.navigate("detail")
+                        Log.d("HomeScreen", "FloatingActionButton clicked")
+                    },
+                    containerColor = MaterialTheme.colorScheme.primary
+                ) {
+                    Icon(Icons.Filled.Add, contentDescription = "Add")
+                }
+            },
+            content = { padding ->
+                Log.d("HomeScreen", "Scaffold content rendering")
+                Column(
                     modifier = Modifier
-                        .padding(16.dp)
-                        .align(Alignment.CenterHorizontally)
-                )
-                GreetingList(modifier = Modifier.fillMaxWidth(), viewModel = viewModel,mainViewModel = mainViewModel)
-            }
-        }
-    )
-}
-
-
-
-@Composable
-fun GreetingList(modifier: Modifier = Modifier, viewModel: GreetingViewModel, mainViewModel: MainViewModel) {
-    val greetings = remember { viewModel.greetings }
-    Log.d("GreetingList", "Number of greetings: ${greetings.size}") // リストのサイズを確認
-    LazyColumn(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(14.dp)
-    ) {
-        items(viewModel.greetings) { greeting ->
-            Log.d("GreetingList", "Rendering item: ${greeting.text}") // 各アイテムの描画確認
-            Row(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
-            ) {
-                Text(
-                    text = "${greeting.text} (${greeting.count})",
-                    style = MaterialTheme.typography.bodyMedium,
-                    fontSize = 20.sp,
-                )
-                IconButton(
-                    onClick = {
-                        val index = viewModel.greetings.indexOf(greeting)
-                        if (index != -1) {
-                            greetings[index].count += 1
-                            mainViewModel.setCount(index, greetings[index].count)
-                            Log.d("GreetingList", "Updated count for ${greeting.text} to ${greetings[index].count}")
-                        }
-                    }
+                        .padding(padding)
+                        .padding(bottom = 64.dp) // ボタンとの重なりを避けるために下に余白を追加
                 ) {
-                    Icon(Icons.Filled.ThumbUp, contentDescription = "Like")
+                    Text(
+                        text = "自己紹介の項目",
+                        style = MaterialTheme.typography.headlineMedium,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .align(Alignment.CenterHorizontally)
+                    )
+                    GreetingList(
+                        modifier = Modifier.fillMaxWidth(),
+                        viewModel = viewModel,
+                        mainViewModel = mainViewModel
+                    )
                 }
             }
-        }
-    }
-}
-
-
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun DetailScreen(navController: NavHostController, viewModel: GreetingViewModel,mainViewModel: MainViewModel) {//新しい項目を追加する画面
-    var newGreeting by remember { mutableStateOf("") }
-
-
-    Scaffold(
-        topBar = {
-            CenterAlignedTopAppBar(
-                title = { Text("追加画面") },
-                navigationIcon = {
-                    IconButton(onClick = { navController.popBackStack() }) {
-                        Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
-                    }
-                }
-            )
-        },
-        content = { padding ->
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(padding)
-                    .padding(16.dp),
-                verticalArrangement = Arrangement.Center
-            ) {
-                TextField(
-                    value = newGreeting,
-                    onValueChange = { newGreeting = it },
-                    label = { Text("新しい項目") },
-                    modifier = Modifier.fillMaxWidth()
-                )
-                Spacer(modifier = Modifier.height(16.dp))
-                Button(
-                    onClick = {
-
-                        if (newGreeting.isNotBlank()) { // newGreetingが空白でない場合のみ追加
-                            viewModel.addGreeting(newGreeting, mainViewModel)
-                            val jsonData = viewModel.serializeGreetings() // ここでデータをシリアライズ
-                            mainViewModel.setGreetingData(jsonData) // DataStore に保存
-                            navController.popBackStack()
-                        }
-                    }
-                ) {
-                    Text("追加")
-                }
-            }
-        }
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun MyAppPreview() {
-    // ダミーの DataStore を作成
-    val dummyDataStore = AppDataStore(LocalContext.current)
-
-    // MainViewModel を直接インスタンス化
-    val dummyMainViewModel = MainViewModel(dummyDataStore)
-
-    // プレビュー用の GreetingViewModel
-    val dummyGreetingViewModel = GreetingViewModel()
-
-    AndroidBootcampIwatePrefTheme {
-        // MyApp に必要な引数を渡して呼び出す
-        MyApp(
-            navController = rememberNavController(),
-            viewModel = dummyGreetingViewModel,
-            mainViewModel = dummyMainViewModel
         )
     }
-}
 
 
+
+
+    @Composable
+    fun GreetingList(
+        modifier: Modifier = Modifier,
+        viewModel: GreetingViewModel,
+        mainViewModel: MainViewModel
+    ) {
+        val greetings = remember { viewModel.greetings }
+        LazyColumn(
+            modifier = modifier
+                .fillMaxSize()
+                .padding(14.dp)
+        ) {
+            items(viewModel.greetings) { greeting ->
+                Row(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    Text(
+                        text = "${greeting.text} (${greeting.count})",
+                        style = MaterialTheme.typography.bodyMedium,
+                        fontSize = 20.sp,
+                    )
+                    IconButton(
+                        onClick = {
+                            val index = viewModel.greetings.indexOf(greeting)
+                            if (index != -1) {
+                                viewModel.greetings[index] = viewModel.greetings[index].copy(count = greetings[index].count + 1)
+                                mainViewModel.setCount(index, greetings[index].count)
+                            }
+                        }
+                    ) {
+                        Icon(Icons.Filled.ThumbUp, contentDescription = "Like")
+                    }
+                }
+            }
+        }
+    }
+
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun DetailScreen(
+        navController: NavHostController,
+        viewModel: GreetingViewModel,
+        mainViewModel: MainViewModel
+    ) {//新しい項目を追加する画面
+        var newGreeting by remember { mutableStateOf("") }
+
+
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = { Text("追加画面") },
+                    navigationIcon = {
+                        IconButton(onClick = { navController.popBackStack() }) {
+                            Icon(Icons.Filled.ArrowBack, contentDescription = "Back")
+                        }
+                    }
+                )
+            },
+            content = { padding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(padding)
+                        .padding(16.dp),
+                    verticalArrangement = Arrangement.Center
+                ) {
+                    TextField(
+                        value = newGreeting,
+                        onValueChange = { newGreeting = it },
+                        label = { Text("新しい項目") },
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Button(
+                        onClick = {
+
+                            if (newGreeting.isNotBlank()) { // newGreetingが空白でない場合のみ追加
+                                viewModel.addGreeting(newGreeting, mainViewModel)
+                                val jsonData = viewModel.serializeGreetings() // ここでデータをシリアライズ
+                                mainViewModel.setGreetingData(jsonData) // DataStore に保存
+                                navController.popBackStack()
+                            }
+                        }
+                    ) {
+                        Text("追加")
+                    }
+                }
+            }
+        )
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun MyAppPreview() {
+        // ダミーの DataStore を作成
+        val dummyDataStore = AppDataStore(LocalContext.current)
+
+        // MainViewModel を直接インスタンス化
+        val dummyMainViewModel = MainViewModel(dummyDataStore)
+
+        // プレビュー用の GreetingViewModel
+        val dummyGreetingViewModel = GreetingViewModel()
+
+        AndroidBootcampIwatePrefTheme {
+            // MyApp に必要な引数を渡して呼び出す
+            MyApp(
+                navController = rememberNavController(),
+                viewModel = dummyGreetingViewModel,
+                mainViewModel = dummyMainViewModel
+            )
+        }
+    }
