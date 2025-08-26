586ec06c5aeff2e76615854eaf1edc1ff33bb789
tsuchida <yu41926t.tutida@icloud.com>
2025-08-26 22:09:42 +0900

    pref:MainViewModelファイルにコメントを追加

--- app/src/main/java/com/example/androidbootcampiwatepref/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/androidbootcampiwatepref/viewmodel/MainViewModel.kt
@@ -7,18 +7,24 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
 
 class MainViewModel(
+    //コンストラクタでAppDataStoreのインスタンスを受け取る
     val dataStore: AppDataStore
+
+    //ViewModelはUI関連のデータを管理し、UIコンポーネントのライフサイクルに対応
 ) : ViewModel() {
+
+    //以下の二つの変数の中身は自動で変更される
     val counts get() = dataStore.counts
     val greetingData: Flow<String?> = dataStore.greetingData
 
-    // AppDataStoreのsetGreetingDataを呼び出す関数
+    // AppDataStoreに挨拶データ（JSON文字列）を非同期で保存する関数
     fun setGreetingData(jsonData: String) {
         viewModelScope.launch {
             dataStore.setGreetingData(jsonData)
         }
     }
 
+    // 指定したインデックスのカウント値を更新する関数
     fun setCount(index: Int, countValue: Int) {
         viewModelScope.launch {
             dataStore.setCount(index, countValue)
